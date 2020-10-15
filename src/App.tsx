6a12c82b4c4dbcf6c9cdcc9947d50a89ced473ea
Prabhu <prabhu.m.murthy@gmail.com>
2020-10-15 12:36:05 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,154 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Chrono } from "react-chrono";
+import fontLoader from "webfontloader";
+import Footer from "./app-footer";
+import { VerticalBasic, VerticalTree, VerticalTreeMixed } from "./app-samples";
+import "./App.css";
+import {
+  ComponentContainer,
+  Description,
+  DescriptionContent,
+  DescriptionHeader,
+  Horizontal,
+  Wrapper,
+} from "./App.styles";
+import AppHeader from "./AppHeader";
+import data from "./data";
+import Features from "./Features";
+import useMediaQuery from "./mediaQueryEffect";
 
-function App() {
+const NewDemo: React.FunctionComponent = () => {
+  const [state, setState] = useState({ fontsLoaded: false, mediaType: "" });
+  const type = useMediaQuery();
+  const [items, setItems] = useState<any[]>([]);
+  const [cardHeight, setCardHeight] = useState(250);
+
+  const setFont = useCallback(() => {
+    fontLoader.load({
+      google: {
+        families: ["Open Sans:300,400,500"],
+      },
+      active: () => {
+        setState({
+          fontsLoaded: true,
+          mediaType: type,
+        });
+      },
+    });
+  }, [type]);
+
+  useEffect(() => {
+    setFont();
+  }, [setFont]);
+
+  useEffect(() => {
+    const newItems = data.map(
+      ({ title, contentTitle, contentText, contentDetailedText }) => ({
+        title,
+        contentTitle,
+        contentText,
+        contentDetailedText,
+      })
+    );
+    setItems(newItems);
+  }, []);
+
+  useEffect(() => {
+    if (type === "desktop") {
+      setCardHeight(250);
+    } else if (type === "big-screen") {
+      setCardHeight(400);
+    } else if (type === "tablet") {
+      setCardHeight(260);
+    } else {
+      setCardHeight(200);
+    }
+  }, [type]);
+
+  const isMobile = useMemo(() => {
+    return state.mediaType !== "desktop" && state.mediaType !== "big-screen";
+  }, [state.mediaType]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Wrapper show={state.fontsLoaded} type={state.mediaType}>
+      <>
+        {/* header */}
+        <AppHeader type={type} />
 
-export default App;
+        {/* project description */}
+        <DescriptionContent style={{ fontSize: "1rem" }}>
+          <strong>react-chrono</strong> is a modern timeline component built for{" "}
+          <a href="https://reactjs.org">React</a>.
+        </DescriptionContent>
+
+        {/* features */}
+        <Features />
+        <br />
+
+        {/* demo description */}
+        <DescriptionContent>
+          Lasting six years and one day, the Second World War started on 1
+          September 1939. Here we trace the timeline of a conflict that engulfed
+          the world.
+        </DescriptionContent>
+
+        {/* Horizontal with Media */}
+        {!isMobile && items.length > 0 && (
+          <Horizontal id="horizontal">
+            <Description>
+              <span>
+                <DescriptionHeader># Horizontal</DescriptionHeader>
+              </span>
+              <DescriptionContent>
+                Timelines are rendered horizontally by default. Use the control
+                buttons or LEFT, RIGHT keys on your keyboard to navigate.
+              </DescriptionContent>
+            </Description>
+            <ComponentContainer type={state.mediaType}>
+              <Chrono
+                items={items}
+                mode="HORIZONTAL"
+                cardHeight={300}
+                slideShow
+                slideItemDuration={1500}
+              />
+            </ComponentContainer>
+          </Horizontal>
+        )}
+
+        {/* Vertical with no Media */}
+        {items.length > 0 && (
+          <VerticalBasic type={state.mediaType} items={items} />
+        )}
+
+        {/* Tree Mode */}
+        {!isMobile && items.length > 0 && (
+          <VerticalTree type={state.mediaType} items={items} />
+        )}
+
+        {/* mixed mode */}
+        {!isMobile && items.length > 0 && (
+          <VerticalTreeMixed type={state.mediaType} cardHeight={cardHeight} />
+        )}
+
+        {/* Horizontal Slideshow */}
+        {/* {!isMobile && (
+          <HorizontalSlideshow type={state.mediaType} cardHeight={cardHeight} />
+        )} */}
+
+        {/* Tree Slideshow */}
+        {/* {!isMobile && items.length > 0 && (
+          <VerticalTreeSlideshow
+            type={state.mediaType}
+            cardHeight={cardHeight}
+          />
+        )} */}
+
+        {/* footer */}
+        <Footer />
+      </>
+    </Wrapper>
+  );
+};
+
+export default NewDemo;
---
+++ src/AppHeader.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { GithubLogo, Header, LogoImage, LogoWrapper } from "./App.styles";
+import AppLogo from "./assets/logo.png";
+
+export default ({ type }: { type: string }) => {
+  return (
+    <Header>
+      <LogoWrapper href="https://react-chrono.vercel.app/">
+        <LogoImage src={AppLogo} />
+      </LogoWrapper>
+
+      <a
+        href="http://github.com/prabhuignoto/react-chrono"
+        target="_new"
+        style={{
+          marginLeft: "auto",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <GithubLogo>
+          <iframe
+            src={`https://ghbtns.com/github-btn.html?user=prabhuignoto&repo=react-chrono&type=star&count=true&size=small`}
+            frameBorder="0"
+            scrolling="0"
+            width="300"
+            height="30"
+            title="GitHub"
+            style={{ position: "absolute" }}
+          ></iframe>
+        </GithubLogo>
+      </a>
+    </Header>
+  );
+};
